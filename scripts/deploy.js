73c5227a6d8fa180bec75abe824fe460d5a4e104
Basha-dude <rebelbasha99@gmail.com>
2023-04-26 22:41:45 +0530

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,17 @@
+
+const hre = require("hardhat");
+
+async function main() {
+ const Lock = await hre.ethers.getContractFactory("Lock");
+  const lock = await Lock.deploy();
+
+  await lock.deployed();
+
+ 
+}
+
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
